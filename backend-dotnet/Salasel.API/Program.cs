6303245de065c5ab0729f030e7bd3686fea0a586
Islam Saeed <elsamsaeed9854@gmail.com>
2026-07-25 15:01:22 +0300

    Configure Seq for logging

--- backend-dotnet/Salasel.API/Program.cs
+++ backend-dotnet/Salasel.API/Program.cs
@@ -24,7 +24,7 @@ Log.Logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
     .WriteTo.Console()
     .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
-    .WriteTo.Seq(builder.Configuration["Seq:Url"] ?? "https://seq.otlob-egy.online")
+    .WriteTo.Seq(builder.Configuration["Seq:Url"] ?? "http://seq:5341")
     .CreateLogger();
 
 builder.Host.UseSerilog();
