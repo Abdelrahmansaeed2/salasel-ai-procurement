fde2623ad17aea7740f5dbfb950af456d1e0ca83
abdelrahmansaeed <abdelrahmansaeed26022020@gmail.com>
2026-08-08 03:36:19 +0300

    Add AuthInterceptor to provideHttpClient to fix 401 Unauthorized errors

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/auth/auth.interceptor';
 
 import { routes } from './app.routes';
 
@@ -8,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
 };
